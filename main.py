a1469975ccfe7e8a46d47bb5facfaf5d442f9324
Khoa Cao <dkhoacao2906@gmail.com>
2025-02-05 13:49:53 +0700

    Update

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
     # Command to run with Gunicorn:
-    # gunicorn -w 4 -k uvicorn.workers.UvicornWorker fastapi_merge_images:app --bind 0.0.0.0:8000
+    # gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:8000
